8e99750996542fa9b440986a3565c8e06ab9b7b5
Lauren Stephanian <lms2221@columbia.edu>
2014-02-14 01:40:44 -0500

    Update index.php

--- index.php
+++ index.php
@@ -14,6 +14,7 @@
 
     <!-- Custom styles for this template -->
     <link href="cover.css" rel="stylesheet">
+    <link href="scrollEffect.css" rel="stylesheet">
 
     <!-- Just for debugging purposes. Don't actually copy this line! -->
     <!--[if lt IE 9]><script src="../../assets/js/ie8-responsive-file-warning.js"></script><![endif]-->
